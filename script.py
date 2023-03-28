cd8064db695d8b3d6090d25371c68814e1f7e386
YaridTheBoi <kacper.chrostowski@gmail.com>
2023-03-28 21:14:44 +0200

    camera and stream tohttp

--- script.py
+++ script.py
@@ -1,19 +1,12 @@
 import cv2
 import numpy as np
 import os.path as path
+from flask import Flask, Response
 
-input_file=input("Nazwa wejscia: ")
-output_file=input("Nazwa wyjscia: ")
 
-if(not path.isfile(input_file)):
-    print("Nie ma takiego pliku wejsciowego")
-    quit()
 
-cap=cv2.VideoCapture(input_file)
-
-fps = cap.get(cv2.CAP_PROP_FPS)
-frame_size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-print(fps, frame_size)
+fps = 25.0  # Przyjmujemy 30 klatek na sekundę jako standard
+frame_size = (640, 480)  # Przyjmujemy rozmiar klatki 640x480 pikseli
 
 
 new_height = frame_size[1]
@@ -21,8 +14,6 @@ new_widht = new_height*2
 
 new_framesize = (new_widht, new_height)
 
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter(output_file, fourcc, fps, new_framesize)
 
 
 center = (new_height // 2, new_height // 2)
@@ -55,13 +46,22 @@ map_y = new_y
 
 
 
-# Pętla po ramkach wideo
-while cap.isOpened():
-    # Odczytaj kolejną ramkę wideo
-    ret, frame = cap.read()
 
-    if ret:
-        # Skopiuj ramkę
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Strumień wideo jest aktywny. Otwórz adres http://localhost:5000/stream w przeglądarce internetowej lub w odtwarzaczu VLC."
+
+def gen():
+    cap = cv2.VideoCapture(0)
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
         cropped_frame = cv2.resize(frame, (new_height, new_height), interpolation=cv2.INTER_AREA)
         copy = cropped_frame.copy()
 
@@ -69,12 +69,16 @@ while cap.isOpened():
         fisheye2 = cv2.remap(copy, map_x, map_y, cv2.INTER_LINEAR)
 
         resize_copy = cv2.hconcat([fisheye1, fisheye2])
-        # Zapisz skopiowaną ramkę do pliku wyjściowego
-        out.write(resize_copy)
 
-    else:
-        break
+        ret, jpeg = cv2.imencode('.jpg', resize_copy)
+        frame = jpeg.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-# Zakończ odtwarzanie pliku i zwolnij zasoby
-cap.release()
-out.release()
+@app.route('/stream')
+def stream():
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
